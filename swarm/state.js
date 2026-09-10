98aa7ff111e3be6e9872e3cc400cf2f3f9bc160f
TheDudoe <mapledevsbusiness@gmail.com>
2026-09-10 19:07:35 +0000

    Update from https://github.com/robindudoe/mapledevs-scraper/commit/310deb03a6d95a2e97713a44c30cc2dea58bfade

--- swarm/state.js
+++ swarm/state.js
@@ -16,7 +16,7 @@ window.SWARM_STATE = {
       "status": "Idle",
       "last_action": "Error: MISSING_API_KEY: Please set GOOGLE_API_KEY or GEMINI_API_KEY in your .env file.",
       "project": "Gaming News & Jobs",
-      "last_run": "2026-09-09T19:18:18.038Z"
+      "last_run": "2026-09-10T19:07:33.146Z"
     },
     "reviewer": {
       "status": "Idle",
